69bb4c94626bec9c954bcf07e8d2f496c8fc9f95
akirilltikhonov <akirilltikhonov@gmail.com>
2019-11-05 22:50:05 +0300

    Добавлен второй вид ошибки при определении ориентации камеры
    Новая ошибка растет с каждым шагом алгоритма. К концу алгоритма ошибка составит 1 градус

--- Algorithm_runs.m
+++ Algorithm_runs.m
@@ -21,29 +21,6 @@ end
 
 t=1:N_MODEL;          %all observations
 l=t/F_frame; 
-%{
-figure
-plot(l,error_XYZ_averaged)
-legend ('Средняя ошибка по координате X за N прогонов','Средняя ошибка по координате Y за N прогонов','Средняя ошибка по координате Z за N прогонов')
-xlabel('Время,с')
-ylabel('Ошибка оценивания,м')
-grid on
-title('Зависимость ошибки оценивания координат особой точки от времени')
-ylim([min(error_XYZ_averaged(:))-1 max(error_XYZ_averaged(:))+1])
-%}
-
-%{
-%% All 3 coordinates
-figure
-plot(l,error_XYZ)
-legend ('Ошибка по координате X для N прогонов','Ошибка по координате Y для N прогонов','Ошибка по координате Z для N прогонов')
-xlabel('Время,с')
-ylabel('Ошибка оценивания,м')
-grid on
-title('Зависимость ошибки оценивания координат особой точки от времени')
-ylim([min(error_XYZ(:))-1 max(error_XYZ(:))+1])
-%}
-
 
 %% X coordinate
 figure
@@ -55,7 +32,7 @@ grid on
 title('Зависимость ошибки оценивания координат особой точки от времени')
 ylim([min(error_Y(:))-1 max(error_Y(:))+1])
 
-%{
+
 %% Y coordinate
 figure
 plot(l,error_Y)
@@ -75,4 +52,14 @@ ylabel('Ошибка оценивания,м')
 grid on
 title('Зависимость ошибки оценивания координат особой точки от времени')
 ylim([min(error_Z(:))-1 max(error_Z(:))+1])
-%}
+
+
+%% All 3 coordinates
+% figure
+% plot(l,error_XYZ)
+% legend ('Ошибка по координате X для N прогонов','Ошибка по координате Y для N прогонов','Ошибка по координате Z для N прогонов')
+% xlabel('Время,с')
+% ylabel('Ошибка оценивания,м')
+% grid on
+% title('Зависимость ошибки оценивания координат особой точки от времени')
+% ylim([min(error_XYZ(:))-1 max(error_XYZ(:))+1])
