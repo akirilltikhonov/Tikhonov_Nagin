775ddd0ac168e5e929ba3d082a5b35fb122185f6
akirilltikhonov <akirilltikhonov@gmail.com>
2019-11-10 15:53:12 +0300

    Переделан алгоритм. Теперь больше функций

---
+++ new/Main.m
@@ -0,0 +1,99 @@
+%%%%
+% point coordinates estimation
+% we know our ENU coordinates
+% we measure point position in video frame - Y1 , Y2
+% we know relationship X -> Y -> Y1,Y2
+% EKF:
+% X = [Zx Zy Zz]
+% Y = [Y1 Y2]
+%%
+clear all
+close all
+clc
+
+%% OPTIONS
+MODEL_TIME_SEC = 100;   % observation time
+F_frame = 24;           % frames per second
+T = 1/F_frame;          % frame duration
+N_MODEL = ceil(MODEL_TIME_SEC/T);   % number of observations
+
+sko_Coordinate_Meas = 0.05;         % RTK solution
+wVu = 0.5;              % radial frequency
+Vku = 2;                % max velocity
+PointZ = [0;1;5];       % true point coordinates in ENU frame
+ENU2RPY = eye(3);       % rotation matrix RPY to ENU
+RPY2ENU = ENU2RPY';     % rotation matrix ENU to RPY
+
+%% Filter and camera initialization
+Point_estim = Point_estim_init(2);
+
+%%
+for k = 1:N_MODEL  
+tt = k*T;
+%% Camera dynamic
+[Xcam, myX] = Dynamic(Vku, wVu, sko_Coordinate_Meas, tt);
+myX_mas(:,k) = myX;     % all points camera position
+
+%% Camera rotation and framepoint
+Tturn=3;                % period of turn
+Ufi1deg = 0;            % amplitude of turn (deg) relative to X
+Ufi2deg = 0;            % amplitude of turn (deg) relative to Y
+Ufi3deg = 60;           % amplitude of turn (deg) relative to Z
+
+%% ENU2RPY error
+%T_error = 30;           
+error_deg = 1;          % by the end of simulation time error wiil be "error_deg" deg
+
+%% Pinhole camera model
+[Y, FramePoint, ENU2RPY_with_error, POINT_RPY] = Camera_rotation(Tturn, Ufi1deg, Ufi2deg, Ufi3deg, error_deg, PointZ, myX, Point_estim, N_MODEL, tt, k);
+
+%% EKF
+[Point_estim, Frame_Point_Y] = EKF (Point_estim, FramePoint, POINT_RPY, Xcam, ENU2RPY_with_error, Y)
+Frame_Point_mas(1:2,k) = Frame_Point_Y;
+
+%% Results
+xoc_mas(:,k)=Point_estim.filter.xoc;            
+error(:,k)=Point_estim.filter.xoc-PointZ;       
+end
+
+%% Camera Movement 
+figure; plot3(myX_mas(1,:), myX_mas(2,:),myX_mas(3,:) ); hold on; plot3(PointZ(1),PointZ(2),PointZ(3),'*'); grid on
+xlabel('X1')
+ylabel('X2')
+zlabel('X3')
+
+%% Trajectory point on the screen
+
+figure
+comet(Frame_Point_mas(1,:), Frame_Point_mas(2,:));
+xlim([-Point_estim.camera.L/2,Point_estim.camera.L/2]);
+ylim([-Point_estim.camera.L/2,Point_estim.camera.L/2]);
+
+figure
+plot(Frame_Point_mas(1,:), Frame_Point_mas(2,:), '*');
+xlim([-Point_estim.camera.L/2,Point_estim.camera.L/2]);
+ylim([-Point_estim.camera.L/2,Point_estim.camera.L/2]);
+
+%%
+
+t=1:k;          %all observations
+l=t/F_frame; 
+
+figure
+plot(l,error)
+legend ('Ошибка по координате x1','Ошибка по координате x2','Ошибка по координате x3')
+xlabel('Время,с')
+ylabel('Ошибка оценивания,м')
+grid on
+title('Зависимость ошибки оценивания координат особой точки от времени')
+ylim([min(error(:))-1 max(error(:))+1])
+
+figure
+plot(l,xoc_mas)
+legend ('x1','x2','x3')
+xlabel('Время,с')
+ylabel('координаты особой точки,м')
+grid on
+title('зависимость изменения координат особой точки от времени')
+
+
