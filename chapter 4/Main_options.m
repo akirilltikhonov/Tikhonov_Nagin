ef822129edfc61c7e48c1a1912a32b21c0d00602
akirilltikhonov <akirilltikhonov@gmail.com>
2019-12-04 13:49:52 +0300

    Поправил

--- chapter 4/Main_options.m
+++ chapter 4/Main_options.m
@@ -8,16 +8,16 @@ Options.N_MODEL = ceil(Options.MODEL_TIME_SEC/Options.T);   % number of observat
 Options.sko_Coordinate_Meas = 0.05;     % RTK solution
 Options.wVu = 0.5;                      % radial frequency
 Options.Vku = 2;                        % max velocity
-Options.PointZ1 = [0;1;5];              % true coordinates of special point1 in ENU frame
-Options.PointZ2 = [0;-1;5];             % true coordinates of special point2 in ENU frame
+Options.PointZ1 = [-3;3;10];              % true coordinates of special point1 in ENU frame
+Options.PointZ2 = [3;-3;10];             % true coordinates of special point2 in ENU frame
 Options.ENU2RPY = eye(3);               % rotation matrix RPY to ENU
 Options.RPY2ENU = Options.ENU2RPY';     % rotation matrix ENU to RPY
 
 %% Camera rotation and framepoint
-Options.Tturn=3;                % period of turn
-Options.Ufi1deg = 0;            % amplitude of turn (deg) relative to X
-Options.Ufi2deg = 60;            % amplitude of turn (deg) relative to Y
-Options.Ufi3deg = 60;           % amplitude of turn (deg) relative to Z
+Options.Tturn=60;                % period of turn
+Options.Ufi1deg = 5;            % amplitude of turn (deg) relative to X
+Options.Ufi2deg = 60*0;            % amplitude of turn (deg) relative to Y
+Options.Ufi3deg = 60*0;           % amplitude of turn (deg) relative to Z
 
 %% ENU2RPY error   
 Options.error_deg = 1;          % by the end of simulation time error wiil be "error_deg" deg
